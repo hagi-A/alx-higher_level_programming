ee52cd9d86b5fc5b08a5487335b56c9a97fb84bc
Hareg <haregalemu9@gmail.com>
2023-03-28 13:17:48 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
+
+
 class Square:
-    """Defines a square"""
+    """ class Square that defines a square"""
     def __init__(self, size):
-        """Initialises the data"""
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
         self.__size = size
